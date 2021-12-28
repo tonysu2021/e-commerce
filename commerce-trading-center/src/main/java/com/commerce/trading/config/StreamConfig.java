4d8ee5bfa380305262c9efed8db0584540e94d71
Tony.su <tony.su@rhf-tech.com>
2021-12-28 15:10:31 +0800

    Fix

--- commerce-trading-center/src/main/java/com/commerce/trading/config/StreamConfig.java
+++ commerce-trading-center/src/main/java/com/commerce/trading/config/StreamConfig.java
@@ -1,18 +1,14 @@
 package com.commerce.trading.config;
 
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.commerce.stream.annotation.EnableCustomMQ;
-import com.commerce.stream.protocol.StreamProtocols;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-@SuppressWarnings("deprecation")
 @Configuration
-@EnableBinding(value = { StreamProtocols.class})
 @EnableCustomMQ
 public class StreamConfig {
 
